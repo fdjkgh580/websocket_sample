1df5ab46e3fa1d18438f53e3ae6d88c98faa37c6
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2017-09-19 16:13:49 +0800

    可以傳送圖片

--- app.js
+++ app.js
@@ -1,14 +1,78 @@
 $(function() {
 
+	$(".form1").on("change", ".upl", function (){
+		var _this = this;
+		
+	    $.filePreview.create(this, {
+	        isReader: true,
+	        progress: function (key, percent){
+	            console.log(percent)
+	        },
+	        success: function (key, obj){
+	           	$(".preview").css("background-image", "url("+obj.preview+")");
+	           	$(".preview").attr("data-base64", obj.base64);
+	        }
+	    });
+
+	});
+
+	$(".form1").on("click", ".preview", function (){
+		$(".upl").click();
+	});
+
+	$(".form1").on("submit", function (){
+
+		var name    = $(this).find('.name').val();
+		var message = $(this).find(".message").val();
+		var img     = $(this).find(".preview").attr("data-base64");
+
+		var data    = {
+			name: name,
+			message: message,
+			img: img
+		};
+
+		// 發送到對方
+		_conn.send(JSON.stringify(data));
+
+		// 也傳到己方
+		chatHelper.add(data)
+		chatHelper.name_lcok();
+
+		return false;
+	});
+
+
 	// 輔助行為類別
 	const chatHelper = new function (){
+		/**
+		 * 在訊息列中，顯示新加入的訊息
+		 * @param obj.name
+		 * @param obj.message
+		 * @param obj.img
+		 */
 		this.add = function (obj){
-			var li = '<li>' + obj.name + " 說：" + obj.message + '</li>';
+
+			if (obj.name == "new_user") {
+				var li = 
+					'<li>' + 
+						"<span>新訪客進入</span>" + 
+					'</li>';
+			}
+			else {
+				var li = 
+					'<li>' + 
+						"<span>" + obj.name + " 說：" + obj.message + "</span>" + 
+						"&emsp;<img src='" + obj.img + "' width='120'>" + 
+					'</li>';
+			}
 			$(".chat").append(li)
-			this.empty_message();
+			this.reset_message();
 		}
-		this.empty_message = function (){
+		this.reset_message = function (){
 			$(".message").val('').focus();
+			$(".upl").val(null);
+			$(".preview").css("background-image", 'url(null)').removeAttr("data-base64")
 		}
 		this.name_lcok = function (){
 			$(".name").prop("disabled", true);
@@ -16,15 +80,24 @@ $(function() {
 		this.debug = function (msg){
 			$(".debug").append(msg + "\n")
 		}
+		this.new_user = function (){
+			_conn.send(JSON.stringify({
+				name: 'new_user',
+				message: '加入聊天',
+				img: null
+			}));
+		}
 	}
 	
 	chatHelper.debug('系統連線中...')
 
 	// WebSocket
-	var _conn = new WebSocket('ws://test.websocket.dennings.org:8080');
+	var _conn = new WebSocket('ws://websocket.localhost:8080');
 
 	$.extend(_conn, {
 		onopen : function(e) {
+			
+			chatHelper.new_user();
 			chatHelper.debug('連線成功。')
 		},
 		onerror : function(e) {
@@ -40,27 +113,9 @@ $(function() {
 	});
 	
 
+
+
 	
-
-
-	$(".form1").on("submit", function (){
-
-		var name    = $(this).find('.name').val();
-		var message = $(this).find(".message").val();
-		var data    = {
-			name: name,
-			message: message
-		};
-
-		// 發送到對方
-		_conn.send(JSON.stringify(data));
-
-		// 也傳到己方
-		chatHelper.add(data)
-		chatHelper.name_lcok();
-
-		return false;
-	})
 
 });
 
