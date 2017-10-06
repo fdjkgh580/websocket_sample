9b76176143fc1afd71058a3857e46699f7ce3a4c
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2017-10-06 12:15:30 +0800

    改用 psr-4 可以解決報錯的問題

--- swoole.php
+++ swoole.php
@@ -1,26 +1,5 @@
-<?php 
-
-class Swoole_helper 
-{
-	/**
-	 * 輔助發送
-	 * @param    object $ws           Websocket 物件
-	 * @param    int    $self         自己的連線編號
-	 * @param    bool   $is_send_self 是否要發送給自己？建議使用 false
-	 * @param    string $data         發送的數據
-	 */
-	static public function push_all($param = []) 
-	{
-		// 取出所有的連線編號
-		foreach ($param['ws']->connections as $fd)
-		{
-			// 若自己發送的數據，不推送給自己
-			if ($fd == $param['self'] and $param['is_send_self'] === false) continue;
-
-			$param['ws']->push($fd, $param['data']);
-		}
-	}
-}
+<?php
+require_once 'vendor/autoload.php'; 
 
 
 
@@ -35,7 +14,7 @@ $ws->on('open', function ($ws, $request) {
 // 監聽 WebSocket 訊息事件
 $ws->on('message', function ($ws, $frame) {
 
- 	Swoole_helper::push_all([
+ 	\Jsnlib\Swoole::push_all([
 		'ws'           => $ws,
 		'self'         => $frame->fd,
 		'is_send_self' => false,
